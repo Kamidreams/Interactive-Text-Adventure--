adcf543b5af70b478311cc0fe3964043eaffaad5
Kamidreams <105478024+Kamidreams@users.noreply.github.com>
2022-09-21 11:05:18 -0700

    Finish path 1 and 2

--- Project-3/Assets/Scripts/SceneLoader.cs
+++ Project-3/Assets/Scripts/SceneLoader.cs
@@ -46,78 +46,73 @@ public class SceneLoader : MonoBehaviour
         SceneManager.LoadScene(7);
     }
 
-    public void Choice15()
+    public void Choice8()
     {
         SceneManager.LoadScene(8);
     }
 
-    public void Choice8()
+    public void Choice9()
     {
         SceneManager.LoadScene(9);
     }
 
-    public void Choice9()
+    public void Choice3()
     {
         SceneManager.LoadScene(10);
     }
 
-    public void Choice3()
+    public void Choice10()
     {
         SceneManager.LoadScene(11);
     }
 
-    public void Choice10()
+public void Choice11()
     {
         SceneManager.LoadScene(12);
     }
 
-public void Choice11()
+public void Choice12()
     {
         SceneManager.LoadScene(13);
     }
 
-public void Choice12()
+public void Choice13()
     {
         SceneManager.LoadScene(14);
     }
 
-public void Choice13()
+public void Choice14()
     {
         SceneManager.LoadScene(15);
     }
-
-public void Choice14()
-    {
-        SceneManager.LoadScene(16);
-    }
 public void Ending5()
     {
-        SceneManager.LoadScene(17);
+        SceneManager.LoadScene(16);
     }
 
     public void Ending8()
     {
-        SceneManager.LoadScene(18);
+        SceneManager.LoadScene(17);
     }
 
     public void Ending9()
     {
-        SceneManager.LoadScene(19);
+        SceneManager.LoadScene(18);
     }
 
     public void Ending12()
     {
-        SceneManager.LoadScene(20);
+        SceneManager.LoadScene(19);
     }
     
     public void Ending15()
     {
-        SceneManager.LoadScene(21);
+        SceneManager.LoadScene(20);
     }
 
  public void GoodEnding()
     {
-        SceneManager.LoadScene(22);
+        SceneManager.LoadScene(21);
     }
 
     public void QuitGame()
